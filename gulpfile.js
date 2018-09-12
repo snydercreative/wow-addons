7d13d333331b3ff3e384cec83b632c3faf8e93fc
snydercreative <me@michaelsnyder.me>
2018-09-12 16:19:27 -0400

    Build system setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,36 @@
-const gulp = require('gulp');
+const fs = require('fs')
+const gulp = require('gulp')
+const zip = require('gulp-zip')
+const clean = require('gulp-clean')
+
+const clientAddonFolder = 'E:\\World of Warcraft\\Interface\\AddOns'
+const base = { base: '.' }
+
+const extractVersion = (tocFile) => {
+	const versionContents = fs.readFileSync(tocFile)
+	const versionRegex = /\d+\.\d+\.\d+/;
+	const matches = versionRegex.exec(versionContents);
+
+	return matches.length ? matches[0] : '';
+};
 
 gulp.task('find-my-buddy', () => {
-	return gulp.src(['FindMyBuddy/**/*'])
-		.pipe(gulp.dest('E:\\World of Warcraft\\Interface\\AddOns\\FindMyBuddy'));
+	const addonName = 'FindMyBuddy'
+	const version = extractVersion(`${addonName}/${addonName}.toc`)
+	const files = `${addonName}/**/*`
+	const zipFileName = `${addonName}-${version}.zip`
+	
+	const cleanZip = gulp.src(zipFileName)
+		.pipe(clean())
+
+	const zipFiles = gulp.src([files], base)
+		.pipe(zip(zipFileName))
+		.pipe(gulp.dest('.'))
+	
+	const deployFiles = gulp.src([files], base)
+		.pipe(gulp.dest(clientAddonFolder))
+
+	return [cleanZip, zipFiles, deployFiles]
 })
 
-gulp.task('default', ['find-my-buddy']);
+gulp.task('default', ['find-my-buddy'])
